2e58c608419143257616bc9a7ff9afe19e5d9f13
amnasj <imammka23@gmail.com>
2025-11-07 13:45:32 +0700

    imam update

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
 // import Homescreen from "@/components/tugas/HomeScreen";
 // import DetailScreen from "@/components/tugas/DetailScreen";
-// import AddScreen from "@/components/tugas/addScreen";
-import UpdateScreen from "@/components/tugas/updateScreen";
+import AddScreen from "@/components/tugas/addScreen";
+// import UpdateScreen from "@/components/tugas/updateScreen";
 
 export default function Index() {
   return (
     <>
       
-      <UpdateScreen />
+      <AddScreen />
     </>
   );
 }
